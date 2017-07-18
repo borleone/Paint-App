cbbc3737aca2ea8fe49421ba4ab93f68a0c9923f
borleone <pushkar.borle@gmail.com>
2017-07-18 16:02:49 -0400

    first commit

---
+++ app/src/main/java/com/borleone/paint/MainActivity.java
@@ -0,0 +1,138 @@
+package com.borleone.paint;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.flask.colorpicker.ColorPickerView;
+import com.flask.colorpicker.OnColorSelectedListener;
+import com.flask.colorpicker.builder.ColorPickerClickListener;
+import com.flask.colorpicker.builder.ColorPickerDialogBuilder;
+
+public class MainActivity extends AppCompatActivity {
+
+    private PaintActivity paintActivity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_main);
+        paintActivity = (PaintActivity) findViewById(R.id.paint);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_clear) {
+            // Clear the view
+            paintActivity.clear();
+            return true;
+        }
+        if (id == R.id.action_color) {
+            // Display dialog of color picker
+            pickColor();
+            return true;
+        }
+        if (id == R.id.action_size) {
+            // Display dialog to select width of strokes
+            changeBrushWidth();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void pickColor() {
+        final Context context = MainActivity.this;
+        ColorPickerDialogBuilder
+                .with(context)
+                .setTitle("Choose color")
+                .initialColor(paintActivity.getColor())
+                .wheelType(ColorPickerView.WHEEL_TYPE.FLOWER)
+                .density(12)
+                .setOnColorSelectedListener(new OnColorSelectedListener() {
+                    @Override
+                    public void onColorSelected(int selectedColor) {
+                        Toast.makeText(getApplicationContext(), "onColorSelected: 0x" + Integer.toHexString(selectedColor), Toast.LENGTH_LONG).show();
+                    }
+                })
+                .setPositiveButton("ok", new ColorPickerClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int selectedColor, Integer[] allColors) {
+                        paintActivity.changeColor(selectedColor);
+                    }
+                })
+                .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                })
+                .build()
+                .show();
+
+    }
+
+    public void changeBrushWidth() {
+        final Dialog brushSizeDialog = new Dialog(this);
+        brushSizeDialog.setTitle("Select Brush Size:");
+        brushSizeDialog.setContentView(R.layout.brush_size);
+
+        final TextView text_brush = (TextView) brushSizeDialog.findViewById(R.id.brush_size);
+        final SeekBar seek_brush = (SeekBar) brushSizeDialog.findViewById(R.id.brush_seek);
+
+        seek_brush.setMax(100);
+        seek_brush.setProgress((int) paintActivity.getStrokeWidth());
+        text_brush.setText(String.valueOf(paintActivity.getStrokeWidth()));
+
+        seek_brush.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            //change to progress
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                text_brush.setText(Integer.toString(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+        Button btn_ok = (Button) brushSizeDialog.findViewById(R.id.btn_ok);
+        btn_ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float chosenSize = seek_brush.getProgress();
+                paintActivity.changeStrokeWidth(chosenSize);
+                brushSizeDialog.dismiss();
+            }
+        });
+
+        brushSizeDialog.show();
+
+    }
+
+}
---
+++ app/src/main/java/com/borleone/paint/PaintActivity.java
@@ -0,0 +1,133 @@
+package com.borleone.paint;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by Borle on 6/3/2017.
+ */
+public class PaintActivity extends View {
+
+    public int width;
+    public int height;
+    private Bitmap mBitmap;
+    private Canvas mCanvas;
+    private Path mPath;
+    private Paint mBitmapPaint, mPaint;
+
+    public PaintActivity(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mPath = new Path();
+        mBitmapPaint = new Paint(Paint.DITHER_FLAG);
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setDither(true);
+        mPaint.setColor(Color.BLUE);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeJoin(Paint.Join.ROUND);
+        mPaint.setStrokeCap(Paint.Cap.ROUND);
+        mPaint.setStrokeWidth(20);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+
+        width = w;
+        height = h;
+
+        mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        mCanvas = new Canvas(mBitmap);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        canvas.drawBitmap(mBitmap, 0, 0, mBitmapPaint);
+        canvas.drawPath(mPath, mPaint);
+    }
+
+    private float mX, mY;
+    private static final float TOUCH_TOLERANCE = 4;
+
+    private void touch_start(float x, float y) {
+        mPath.reset();
+        mPath.moveTo(x, y);
+        mX = x;
+        mY = y;
+    }
+
+    private void touch_move(float x, float y) {
+        float dx = Math.abs(x - mX);
+        float dy = Math.abs(y - mY);
+        if (dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE) {
+            mPath.quadTo(mX, mY, (x + mX) / 2, (y + mY) / 2);
+            mX = x;
+            mY = y;
+
+        }
+    }
+
+    private void touch_up() {
+        mPath.lineTo(mX, mY);
+        mCanvas.drawPath(mPath, mPaint);
+        mPath.reset();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float x = event.getX();
+        float y = event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                touch_start(x, y);
+                invalidate();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                touch_move(x, y);
+                invalidate();
+                break;
+            case MotionEvent.ACTION_UP:
+                touch_up();
+                invalidate();
+                break;
+        }
+        return true;
+    }
+
+    public void clear() {
+        setDrawingCacheEnabled(false);
+        onSizeChanged(width, height, width, height);
+        invalidate();
+
+        setDrawingCacheEnabled(true);
+    }
+
+    public void changeColor(int color){
+        mPaint.setColor(color);
+    }
+
+    public int getColor(){
+        return mPaint.getColor();
+    }
+
+    public void changeStrokeWidth(float size){
+        mPaint.setStrokeWidth(size);
+    }
+
+    public float getStrokeWidth(){
+        return mPaint.getStrokeWidth();
+    }
+
+}
+
+
